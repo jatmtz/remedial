af21e1651353c19123954be68d511ab125125773
jatmtz <jatmtz1215@gmail.com>
2024-04-23 11:22:15 -0600

    cambios en component juego

--- src/app/COMPONENTES/juego/juego.component.ts
+++ src/app/COMPONENTES/juego/juego.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
 import { AnimationBuilder, AnimationPlayer } from '@angular/animations';
 import { animate, style } from '@angular/animations';
 import Echo from 'laravel-echo';
@@ -12,6 +12,11 @@ import Pusher from 'pusher-js';
   styleUrl: './juego.component.css'
 })
 export class JuegoComponent implements OnInit{
+
+  constructor(private renderer: Renderer2, private elementRef: ElementRef) {
+
+  }
+
   ngOnInit(){
     
     (window as any).Pusher = Pusher;
@@ -28,8 +33,25 @@ export class JuegoComponent implements OnInit{
     .listen('.barco', (data: any) => {
       console.log(data);
       console.log('hola');
-      
+      if (data.player === 1){
+        this.iniciarAnimacion1();
+      } else if (data.player === 2) {
+        this.iniciarAnimacion2();
+      }
+    
     });
+  }
+
+  iniciarAnimacion1(){
+    const imagen = this.elementRef.nativeElement.querySelector('.containerImagen');
+
+    this.renderer.addClass(imagen, 'moverBarco');
+  }
+
+  iniciarAnimacion2(){
+    const imagen = this.elementRef.nativeElement.querySelector('.containerImagen');
+
+    this.renderer.addClass(imagen, 'moverBarco');
   }
 
 
